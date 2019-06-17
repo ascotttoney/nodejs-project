a98c7025bd7a0a8cbce848b5156e30d8f5e552c9
ascotttoney <ascotttoney@gmail.com>
2019-06-16 21:38:23 -0400

    First commit.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const urlEncodedParser = bodyParser.urlencoded({extended: false})
+
+app.set('view engine', 'ejs')
+app.use('/assets', express.static('assets'))
+// app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', urlEncodedParser, (req, res) => {
+  console.log(req.body)
+  res.render('welcome', {user: req.body})
+})
+
+app.get('/welcome', (req, res) => {
+  res.render('welcome', {user: req.query})
+})
+
+app.listen(8080)
+console.log('Listening on port 8080.')
