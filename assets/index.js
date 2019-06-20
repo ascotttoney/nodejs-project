e0556ea4bc95862ad93d070df674204d3fb5c774
ascotttoney <ascotttoney@gmail.com>
2019-06-19 23:22:17 -0400

    Wednesday evening.

--- assets/index.js
+++ assets/index.js
@@ -1,30 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  form = document.getElementById('new-user-form')
-  form.addEventListener('submit', handleFormSubmit)
+  document.addEventListener('submit', handleFormSubmit)
 
   function handleFormSubmit(e) {
-    e.preventDefault()
+    // e.preventDefault()
 
     const user = {
-      firstName: e.target.children[0].value,
-      lastName: e.target.children[2].value,
-      favorite: e.target.children[4].value
+      lastName: e.target.children[0].value,
+      favorite: e.target.children[2].value
     }
+
     JSON.stringify(user)
-    console.log(user)
+    // console.log(user)
   }
-
-
-  // Code for time of day and such
-  let now = new Date()
-  let hour = now.getHours()
-  let minute = now.getMinutes()
-  let currentTime = `${hour}${minute}`
-
-  let time
-  if (currentTime <= 1159) time = 'morning'
-  else if (currentTime >= 1200 && currentTime > 1700) time = 'afternoon'
-  else time = 'evening'
-
 })
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
+const bodyParser = require('body-parser')
 
 const urlEncodedParser = bodyParser.urlencoded({extended: false})
 
@@ -8,17 +8,51 @@ app.set('view engine', 'ejs')
 app.use('/assets', express.static('assets'))
 app.use(bodyParser.json())
 
+
+// --- TIME STUFF --- //
+
+let date = new Date()
+let hours = date.getHours();
+let minutes = date.getMinutes()
+let ampm = hours >= 12 ? 'pm' : 'am'
+hours = hours % 12
+hours = hours ? hours : 12
+minutes = minutes < 10 ? '0'+minutes : minutes
+let strTime = hours + ':' + minutes + ' ' + ampm
+
+let now = new Date()
+let hour = now.getHours()
+let minute = now.getMinutes()
+let currentTime = `${hour}${minute}`
+
+let time
+if (currentTime <= 1159) time = 'morning'
+else if (currentTime <= 1200 && currentTime > 1700) time = 'afternoon'
+else time = 'evening'
+
+app.locals.strTime = strTime
+app.locals.time = time
+
+
+// --- ROUTING --- //
+
+app.get('/greeting', (req, res) => {
+  // console.log(req.query)
+  res.send(`Good ${time}, Mr. ${req.query.name}!\nThe time is now ${strTime}.\nPut on some ${req.query.favorite} and have a great day. \n\n `)
+})
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/', urlEncodedParser, (req, res) => {
-  res.render('welcome', {user: req.body})
+  // console.log(req.body)
+  app.locals.name = req.body.lastName
+  app.locals.favorite = req.body.favorite
+  res.render('greeting', {user: req.body})
 })
 
-app.get('/welcome', (req, res) => {
-  res.render('welcome', {user: req.query})
-})
+// --- //
 
 app.listen(8080)
 console.log('Listening on port 8080.')
